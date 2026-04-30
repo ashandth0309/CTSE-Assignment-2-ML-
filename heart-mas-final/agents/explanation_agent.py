f3f826e8b22abfa6d53b1ec5cdc0c6709a830d8c
sobiyaantonsuresh <sobiyaantonsuresh@gmail.com>
2026-04-30 22:52:24 +0530

    update my files

---
+++ heart-mas-final/agents/explanation_agent.py
@@ -0,0 +1,11 @@
+from crewai import Agent
+from tools.explanation_tool import explain
+
+explanation_agent = Agent(
+    role="Medical Advisor",
+    goal="Explain prediction results clearly",
+    backstory="Doctor-like assistant for patient guidance",
+    tools=[explain],
+    verbose=True,
+    llm="ollama/phi3"
+)
